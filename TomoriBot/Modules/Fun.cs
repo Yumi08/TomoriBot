589bfe6dee5f6b770d373a49432f78684ccc1647
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-03-30 17:30:58 -0400

    Added some commands

---
+++ TomoriBot/Modules/Fun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TomoriBot.Modules
+{
+	public class Fun : ModuleBase<SocketCommandContext>
+	{
+		[Command("pick")]
+		[Alias("choose")]
+		public async Task Choose([Remainder]string input)
+		{
+			var args = input.Split(' ');
+
+			await Context.Channel.SendMessageAsync(args[Global.R.Next(args.Length)]);
+		}
+	}
+}
---
+++ TomoriBot/Modules/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TomoriBot.Modules
+{
+	public class Utility : ModuleBase<SocketCommandContext>
+	{
+		[Command("ping")]
+		public async Task Ping()
+		{
+			var m = await Context.Channel.SendMessageAsync("Ping...");
+
+			await m.ModifyAsync(msg => msg.Content = $"Pong! The server latency is {m.CreatedAt.Millisecond - Context.Message.CreatedAt.Millisecond}ms!\n" +
+			                                         $"And the API latency is {Context.Client.Latency}ms.");
+		}
+
+		[Command("restart")]
+		public async Task Restart()
+		{
+			if (Context.User.Id != 218429853144186883)
+			{
+				await Context.Channel.SendMessageAsync("Wat u think ur doin <:MiyanoDead:407275770151436289>");
+				return;
+			}
+
+			await Context.Channel.SendMessageAsync("Restarting!");
+
+			System.Diagnostics.Process.Start(
+				"C:\\Users\\Yumi\\source\\Console\\Discord\\TomoriBot\\TomoriBot\\bin\\Release\\TomoriBot.exe");
+			Environment.Exit(0);
+		}
+	}
+}
