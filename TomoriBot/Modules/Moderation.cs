5ce3813f5ed857246733a9b704a9937811a4d8d1
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-04-01 22:03:39 -0400

    Added moderation commands

--- TomoriBot/Modules/Moderation.cs
+++ TomoriBot/Modules/Moderation.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using static TomoriBot.Utilities;
 
 namespace TomoriBot.Modules
 {
@@ -8,9 +10,40 @@ namespace TomoriBot.Modules
 	{
 		[Command("nickname")]
 		[Alias("nick")]
+		[RequireUserPermission(GuildPermission.ManageNicknames)]
 		public async Task Nickname(SocketGuildUser user, [Remainder]string newName)
 		{
 			await Context.Guild.GetUser(user.Id).ModifyAsync(x => { x.Nickname = newName; });
+		}
+
+		[Command("kick")]
+		[RequireUserPermission(GuildPermission.KickMembers)]
+		public async Task Kick(SocketGuildUser user)
+		{
+			await user.KickAsync();
+			await Context.Channel.SendMessageAsync($"{GetNickname(user)} ({user.Id}) has been kicked!");
+		}
+		[Command("kick")]
+		[RequireUserPermission(GuildPermission.KickMembers)]
+		public async Task Kick(SocketGuildUser user, [Remainder]string reason)
+		{
+			await user.KickAsync(reason);
+			await Context.Channel.SendMessageAsync($"{GetNickname(user)} ({user.Id}) has been kicked!");
+		}
+
+		[Command("ban")]
+		[RequireUserPermission(GuildPermission.BanMembers)]
+		public async Task Ban(SocketGuildUser user)
+		{
+			await Context.Guild.AddBanAsync(user);
+			await Context.Channel.SendMessageAsync($"{GetNickname(user)} ({user.Id}) has been banned!");
+		}
+		[Command("ban")]
+		[RequireUserPermission(GuildPermission.BanMembers)]
+		public async Task Ban(SocketGuildUser user, [Remainder]string reason)
+		{
+			await Context.Guild.AddBanAsync(user, reason: reason);
+			await Context.Channel.SendMessageAsync($"{GetNickname(user)} ({user.Id}) has been banned!");
 		}
 	}
 }
