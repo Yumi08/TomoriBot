8233c31b09e740e56e6ba2ee9f0b58110ffa0b63
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-04-02 17:26:18 -0400

    Made major improvemtns and bugfixes to $Danbooru

--- TomoriBot/Modules/Media.cs
+++ TomoriBot/Modules/Media.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using Discord.Commands;
+using Newtonsoft.Json;
 
 namespace TomoriBot.Modules
 {
@@ -20,9 +22,50 @@ namespace TomoriBot.Modules
 			else if (tags.Length == 2) tagsString = $"{tags[0]}+{tags[1]}";
 			else tagsString = "";
 
-			var url = $"http://danbooru.donmai.us/posts/random?tags={tagsString}";
+			var url = $"http://danbooru.donmai.us/posts/random.json?tags={tagsString}";
 
-			await Context.Channel.SendMessageAsync(url);
+			string json = "";
+			using (WebClient client = new WebClient())
+			{
+				json = client.DownloadString(url);
+			}
+
+			var dataObject = JsonConvert.DeserializeObject<dynamic>(json);
+
+			await Context.Channel.SendMessageAsync($"http://danbooru.donmai.us/posts/{dataObject["id"]}");
+		}
+
+		[Command("danbooruf")]
+		public async Task Danbooru(ushort amt, [Remainder] string input)
+		{
+			string[] tags = input.Split(' ');
+			if (tags.Length > 2)
+			{
+				await Context.Channel.SendMessageAsync("Cannot use more than 2 tags!");
+				return;
+			}
+
+			string tagsString;
+			if (tags.Length == 1) tagsString = tags[0];
+			else if (tags.Length == 2) tagsString = $"{tags[0]}+{tags[1]}";
+			else tagsString = "";
+
+			for (int i = 0; i < amt; i++)
+			{
+				var url = $"http://danbooru.donmai.us/posts/random.json?tags={tagsString}";
+
+				string json = "";
+				using (WebClient client = new WebClient())
+				{
+					json = client.DownloadString(url);
+				}
+
+				var dataObject = JsonConvert.DeserializeObject<dynamic>(json);
+
+				await Context.Channel.SendMessageAsync($"http://danbooru.donmai.us/posts/{dataObject["id"]}");
+				await Task.Delay(1000);
+			}
+
 		}
 	}
 }
